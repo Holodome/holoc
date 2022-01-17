5ded8c25a929e5a11975017d9789fbac58e47f83
holodome <ilal.vinogradov@gmail.com>
2022-01-17 23:55:39 +0300

    create c_lang and put there functionality and data related to c language constructs like keywords; implement converting from preprocessor punctuators to c punctuators

---
+++ src/c_lang.c
@@ -0,0 +1,411 @@
+#include "c_lang.h"
+
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "allocator.h"
+#include "buffer_writer.h"
+#include "c_types.h"
+#include "pp_lexer.h"
+#include "str.h"
+#include "unicode.h"
+
+static string KEYWORD_STRINGS[] = {
+    WRAP_Z("(unknown)"),
+    WRAP_Z("auto"),
+    WRAP_Z("break"),
+    WRAP_Z("case"),
+    WRAP_Z("char"),
+    WRAP_Z("const"),
+    WRAP_Z("continue"),
+    WRAP_Z("default"),
+    WRAP_Z("do"),
+    WRAP_Z("double"),
+    WRAP_Z("else"),
+    WRAP_Z("enum"),
+    WRAP_Z("extern"),
+    WRAP_Z("float"),
+    WRAP_Z("for"),
+    WRAP_Z("goto"),
+    WRAP_Z("if"),
+    WRAP_Z("inline"),
+    WRAP_Z("int"),
+    WRAP_Z("long"),
+    WRAP_Z("register"),
+    WRAP_Z("restrict"),
+    WRAP_Z("return"),
+    WRAP_Z("short"),
+    WRAP_Z("signed"),
+    WRAP_Z("unsigned"),
+    WRAP_Z("static"),
+    WRAP_Z("struct"),
+    WRAP_Z("switch"),
+    WRAP_Z("typedef"),
+    WRAP_Z("union"),
+    WRAP_Z("unsigned"),
+    WRAP_Z("void"),
+    WRAP_Z("volatile"),
+    WRAP_Z("while"),
+    WRAP_Z("_Alignas"),
+    WRAP_Z("_Alignof"),
+    WRAP_Z("_Atomic"),
+    WRAP_Z("_Bool"),
+    WRAP_Z("_Complex"),
+    WRAP_Z("_Decimal128"),
+    WRAP_Z("_Decimal32"),
+    WRAP_Z("_Decimal64"),
+    WRAP_Z("_Generic"),
+    WRAP_Z("_Imaginary"),
+    WRAP_Z("_Noreturn"),
+    WRAP_Z("_Static_assert"),
+    WRAP_Z("_Thread_local"),
+    WRAP_Z("_Pragma"),
+};
+
+static string PUNCTUATOR_STRINGS[] = {
+    WRAP_Z("(unknown)"), WRAP_Z(">>="), WRAP_Z("<<="), WRAP_Z("+="),
+    WRAP_Z("-="),        WRAP_Z("*="),  WRAP_Z("/="),  WRAP_Z("%="),
+    WRAP_Z("&="),        WRAP_Z("|="),  WRAP_Z("^="),  WRAP_Z("++"),
+    WRAP_Z("--"),        WRAP_Z(">>"),  WRAP_Z("<<"),  WRAP_Z("&&"),
+    WRAP_Z("||"),        WRAP_Z("=="),  WRAP_Z("!="),  WRAP_Z("<="),
+    WRAP_Z(">="),
+};
+
+static string
+get_kw_str(c_keyword_kind kind) {
+    assert(kind < (sizeof(KEYWORD_STRINGS) / sizeof(*KEYWORD_STRINGS)));
+    return KEYWORD_STRINGS[kind];
+}
+
+// NOTE: Only called for multisymbol punctuators (ot ASCII ones)
+static string
+get_punct_str(c_punct_kind kind) {
+    assert(kind > 0x100);
+    assert((kind - 0x100) <
+           (sizeof(PUNCTUATOR_STRINGS) / sizeof(*PUNCTUATOR_STRINGS)));
+    return PUNCTUATOR_STRINGS[kind - 0x100];
+}
+
+static c_punct_kind
+get_c_punct_kind_from_pp(pp_punct_kind kind) {
+    c_punct_kind result = 0;
+    if (kind < 0x100) {
+        result = (uint32_t)kind;
+    } else {
+        switch (kind) {
+        default:
+            assert(false);
+        case PP_TOK_PUNCT_IRSHIFT:
+            result = C_PUNCT_IRSHIFT;
+            break;
+        case PP_TOK_PUNCT_ILSHIFT:
+            result = C_PUNCT_ILSHIFT;
+            break;
+        case PP_TOK_PUNCT_IADD:
+            result = C_PUNCT_IADD;
+            break;
+        case PP_TOK_PUNCT_ISUB:
+            result = C_PUNCT_ISUB;
+            break;
+        case PP_TOK_PUNCT_IMUL:
+            result = C_PUNCT_IMUL;
+            break;
+        case PP_TOK_PUNCT_IDIV:
+            result = C_PUNCT_IDIV;
+            break;
+        case PP_TOK_PUNCT_IMOD:
+            result = C_PUNCT_IMOD;
+            break;
+        case PP_TOK_PUNCT_IAND:
+            result = C_PUNCT_IAND;
+            break;
+        case PP_TOK_PUNCT_IOR:
+            result = C_PUNCT_IOR;
+            break;
+        case PP_TOK_PUNCT_IXOR:
+            result = C_PUNCT_IXOR;
+            break;
+        case PP_TOK_PUNCT_INC:
+            result = C_PUNCT_INC;
+            break;
+        case PP_TOK_PUNCT_DEC:
+            result = C_PUNCT_DEC;
+            break;
+        case PP_TOK_PUNCT_RSHIFT:
+            result = C_PUNCT_RSHIFT;
+            break;
+        case PP_TOK_PUNCT_LSHIFT:
+            result = C_PUNCT_LSHIFT;
+            break;
+        case PP_TOK_PUNCT_LAND:
+            result = C_PUNCT_LAND;
+            break;
+        case PP_TOK_PUNCT_LOR:
+            result = C_PUNCT_LOR;
+            break;
+        case PP_TOK_PUNCT_EQ:
+            result = C_PUNCT_EQ;
+            break;
+        case PP_TOK_PUNCT_NEQ:
+            result = C_PUNCT_NEQ;
+            break;
+        case PP_TOK_PUNCT_LEQ:
+            result = C_PUNCT_LEQ;
+            break;
+        case PP_TOK_PUNCT_GEQ:
+            result = C_PUNCT_GEQ;
+            break;
+        }
+    }
+    return result;
+}
+
+uint32_t
+fmt_token(token *tok, char *buf, uint32_t buf_size) {
+    buffer_writer w = {buf, buf + buf_size};
+    switch (tok->kind) {
+    case TOK_EOF:
+        break;
+    case TOK_ID:
+        buf_write(&w, "%.*s", tok->str.len, tok->str.data);
+        break;
+    case TOK_NUM:
+        if (c_type_is_int(tok->type->kind)) {
+            buf_write(&w, "%llu", tok->uint_value);
+            switch (tok->type->kind) {
+            default:
+                break;
+            case C_TYPE_ULLINT:
+            case C_TYPE_SLLINT:
+                buf_write(&w, "ll");
+                break;
+            case C_TYPE_ULINT:
+            case C_TYPE_SLINT:
+                buf_write(&w, "l");
+                break;
+            }
+            if (c_type_is_int_unsigned(tok->type->kind)) {
+                buf_write(&w, "u");
+            }
+        } else {
+            buf_write(&w, "%Lg", tok->float_value);
+            switch (tok->type->kind) {
+            default:
+                break;
+            case C_TYPE_FLOAT:
+                buf_write(&w, "f");
+                break;
+            case C_TYPE_LDOUBLE:
+                buf_write(&w, "l");
+                break;
+            }
+        }
+        break;
+    case TOK_STR:
+        assert(tok->type->kind == C_TYPE_ARRAY);
+        switch (tok->type->ptr_to->kind) {
+        default:
+            buf_write(&w, "\"");
+            break;
+        case C_TYPE_WCHAR:
+            buf_write(&w, "L\"");
+            break;
+        case C_TYPE_UCHAR:
+            buf_write(&w, "u8\"");
+            break;
+        case C_TYPE_CHAR16:
+            buf_write(&w, "u\"");
+            break;
+        case C_TYPE_CHAR32:
+            buf_write(&w, "U\"");
+            break;
+        }
+        buf_write(&w, "%.*s", tok->str.len, tok->str.data);
+        buf_write(&w, "\"");
+        break;
+    case TOK_PUNCT:
+        if (tok->punct < 0x100) {
+            buf_write(&w, "%c", tok->punct);
+        } else {
+            string punct_str = get_punct_str(tok->punct);
+            buf_write(&w, "%.*s", punct_str.len, punct_str.data);
+        }
+        break;
+    case TOK_KW: {
+        string kw_str = get_kw_str(tok->kw);
+        buf_write(&w, "%.*s", kw_str.len, kw_str.data);
+    } break;
+    }
+    return w.cursor - buf;
+}
+
+uint32_t
+fmt_tok_verbose(token *tok, char *buf, uint32_t buf_size) {
+    uint32_t result = 0;
+    switch (tok->kind) {
+    case TOK_EOF:
+        result = snprintf(buf, buf_size, "<EOF>");
+        break;
+    case TOK_ID:
+        result = snprintf(buf, buf_size, "<ID>");
+        break;
+    case TOK_NUM:
+        result = snprintf(buf, buf_size, "<Num>");
+        break;
+    case TOK_STR:
+        result = snprintf(buf, buf_size, "<Str>");
+        break;
+    case TOK_PUNCT:
+        result = snprintf(buf, buf_size, "<Punct>");
+        break;
+    case TOK_KW:
+        result = snprintf(buf, buf_size, "<Kw>");
+        break;
+    }
+    buf += result;
+    buf_size -= result;
+    result += fmt_token(tok, buf, buf_size);
+    return result;
+}
+
+token
+convert_pp_token(pp_token *pp_tok, struct allocator *a) {
+    token tok = {0};
+    switch (pp_tok->kind) {
+    case PP_TOK_EOF: {
+        tok.kind = TOK_EOF;
+    } break;
+    case PP_TOK_ID: {
+        tok.kind = TOK_ID;
+        tok.str  = string_dup(a, pp_tok->str);
+    } break;
+    case PP_TOK_NUM: {
+        tok.kind = TOK_NUM;
+    } break;
+    case PP_TOK_STR: {
+        // First, deduce the type
+        switch (pp_tok->str_kind) {
+        default:
+            assert(false);
+            break;
+        case PP_TOK_STR_SCHAR:
+            tok.type = make_array_type(get_standard_type(C_TYPE_CHAR),
+                                       pp_tok->str.len + 1, a);
+            break;
+        case PP_TOK_STR_SUTF8:
+            tok.type = make_array_type(get_standard_type(C_TYPE_UCHAR),
+                                       pp_tok->str.len + 1, a);
+            break;
+        case PP_TOK_STR_SUTF16:
+            // FIXME: Array size
+            tok.type = make_array_type(get_standard_type(C_TYPE_CHAR16),
+                                       pp_tok->str.len + 1, a);
+            break;
+        case PP_TOK_STR_SUTF32:
+            // FIXME: Array size
+            tok.type = make_array_type(get_standard_type(C_TYPE_CHAR32),
+                                       pp_tok->str.len + 1, a);
+            break;
+        case PP_TOK_STR_SWIDE:
+            // FIXME: Array size
+            tok.type = make_array_type(get_standard_type(C_TYPE_WCHAR),
+                                       pp_tok->str.len + 1, a);
+            break;
+        case PP_TOK_STR_CCHAR:
+            tok.type = get_standard_type(C_TYPE_CHAR);
+            break;
+        case PP_TOK_STR_CUTF8:
+            tok.type = get_standard_type(C_TYPE_UCHAR);
+            break;
+        case PP_TOK_STR_CUTF16:
+            tok.type = get_standard_type(C_TYPE_CHAR16);
+            break;
+        case PP_TOK_STR_CUTF32:
+            tok.type = get_standard_type(C_TYPE_CHAR32);
+            break;
+        case PP_TOK_STR_CWIDE:
+            tok.type = get_standard_type(C_TYPE_WCHAR);
+            break;
+        }
+
+        switch (pp_tok->str_kind) {
+        default:
+            assert(false);
+            break;
+        case PP_TOK_STR_SCHAR:
+        case PP_TOK_STR_SUTF8:
+        case PP_TOK_STR_SUTF16:
+        case PP_TOK_STR_SUTF32:
+        case PP_TOK_STR_SWIDE: {
+            uint32_t byte_stride = tok.type->size;
+
+            uint32_t len = 0;
+            char *cursor = pp_tok->str.data;
+            while (*cursor) {
+                uint32_t cp;
+                cursor = utf8_decode(cursor, &cp);
+                ++len;
+            }
+            void *buffer       = aalloc(a, byte_stride * (len + 1));
+            void *write_cursor = buffer;
+            cursor             = pp_tok->str.data;
+            while (*cursor) {
+                uint32_t cp;
+                cursor = utf8_decode(cursor, &cp);
+                if (byte_stride == 1) {
+                    // TODO: If resulting string is utf8 we can skip decodind
+                    // and encoding
+                    write_cursor = utf8_encode(write_cursor, cp);
+                } else if (byte_stride == 2) {
+                    write_cursor = utf16_encode(write_cursor, cp);
+                } else if (byte_stride == 4) {
+                    memcpy(write_cursor, &cp, 4);
+                    write_cursor = (char *)write_cursor + 4;
+                } else {
+                    assert(false);
+                }
+            }
+            uint32_t zero = 0;
+            memcpy(write_cursor, &zero, byte_stride);
+
+            tok.kind = TOK_STR;
+            tok.str  = string(buffer, byte_stride * len);
+        } break;
+        case PP_TOK_STR_CCHAR:
+        case PP_TOK_STR_CUTF8:
+        case PP_TOK_STR_CUTF16:
+        case PP_TOK_STR_CUTF32:
+        case PP_TOK_STR_CWIDE: {
+            // Treat all character literals as mutltibyte.
+            // Convert it to number
+            uint64_t value       = 0;
+            char *cursor         = pp_tok->str.data;
+            uint32_t byte_stride = tok.type->size;
+            while (*cursor) {
+                uint32_t cp;
+                cursor = utf8_decode(cursor, &cp);
+                if (cp >= 1 << (8 * byte_stride)) {
+                    // TODO: Diagnostic
+                    break;
+                }
+                value = (value << (8 * byte_stride)) | cp;
+            }
+            // Make sure the value is in expected range
+            value &= (1 << (8 * byte_stride)) - 1;
+            tok.kind       = TOK_NUM;
+            tok.uint_value = value;
+        } break;
+        }
+    } break;
+    case PP_TOK_PUNCT: {
+        tok.kind = TOK_PUNCT;
+        tok.punct= get_c_punct_kind_from_pp(pp_tok->punct_kind);
+        assert(tok.punct);
+    } break;
+    case PP_TOK_OTHER: {
+        assert(false);
+    } break;
+    }
+    return tok;
+}
--- src/preprocessor.h
+++ src/preprocessor.h
@@ -42,103 +42,6 @@ typedef struct pp_conditional_include {
     bool is_after_else;
     struct pp_conditional_include *next;
 } pp_conditional_include;
-
-// TODO: This is not the place for this language-realted structs and
-// enumerations
-
-typedef enum {
-    C_KW_AUTO          = 0x1,   // auto
-    C_KW_BREAK         = 0x2,   // break
-    C_KW_CASE          = 0x3,   // case
-    C_KW_CHAR          = 0x4,   // char
-    C_KW_CONST         = 0x5,   // const
-    C_KW_CONTINUE      = 0x6,   // continue
-    C_KW_DEFAULT       = 0x7,   // default
-    C_KW_DO            = 0x8,   // do
-    C_KW_DOUBLE        = 0x9,   // double
-    C_KW_ELSE          = 0xA,   // else
-    C_KW_ENUM          = 0xB,   // enum
-    C_KW_EXTERN        = 0xC,   // extern
-    C_KW_FLOAT         = 0xD,   // float
-    C_KW_FOR           = 0xE,   // for
-    C_KW_GOTO          = 0xF,   // goto
-    C_KW_IF            = 0x10,  // if
-    C_KW_INLINE        = 0x11,  // inline
-    C_KW_INT           = 0x12,  // int
-    C_KW_LONG          = 0x13,  // long
-    C_KW_REGISTER      = 0x14,  // register
-    C_KW_RESTRICT      = 0x15,  // restrict
-    C_KW_RETURN        = 0x16,  // return
-    C_KW_SHORT         = 0x17,  // short
-    C_KW_SIGNED        = 0x18,  // signed
-    C_KW_SIZEOF        = 0x19,  // unsigned
-    C_KW_STATIC        = 0x1A,  // static
-    C_KW_STRUCT        = 0x1B,  // struct
-    C_KW_SWITCH        = 0x1C,  // switch
-    C_KW_TYPEDEF       = 0x1D,  // typedef
-    C_KW_UNION         = 0x1E,  // union
-    C_KW_UNSIGNED      = 0x1F,  // unsigned
-    C_KW_VOID          = 0x20,  // void
-    C_KW_VOLATILE      = 0x21,  // volatile
-    C_KW_WHILE         = 0x22,  // while
-    C_KW_ALIGNAS       = 0x23,  // _Alignas
-    C_KW_ALIGNOF       = 0x24,  // _Alignof
-    C_KW_ATOMIC        = 0x25,  // _Atomic
-    C_KW_BOOL          = 0x26,  // _Bool
-    C_KW_COMPLEX       = 0x27,  // _Complex
-    C_KW_DECIMAL128    = 0x28,  // c2x _Decimal128
-    C_KW_DECIMAL32     = 0x29,  // c2x _Decimal32
-    C_KW_DECIMAL64     = 0x2A,  // c2x _Decimal64
-    C_KW_GENERIC       = 0x2B,  // _Generic
-    C_KW_IMAGINARY     = 0x2C,  // _Imaginary
-    C_KW_NORETURN      = 0x2D,  // _Noreturn
-    C_KW_STATIC_ASSERT = 0x2E,  // _Static_assert
-    C_KW_THREAD_LOCAL  = 0x2F,  // _Thread_local
-    C_KW_PRAGMA        = 0x30,  // _Pragma
-} c_keyword_kind;
-
-typedef enum {
-    C_PUNCT_IRSHIFT = 0x101,  // >>=
-    C_PUNCT_ILSHIFT = 0x102,  // <<=
-    C_PUNCT_IADD    = 0x103,  // +=
-    C_PUNCT_ISUB    = 0x104,  // -=
-    C_PUNCT_IMUL    = 0x105,  // *=
-    C_PUNCT_IDIV    = 0x106,  // /=
-    C_PUNCT_IMOD    = 0x107,  // %=
-    C_PUNCT_IAND    = 0x108,  // &=
-    C_PUNCT_IOR     = 0x109,  // |=
-    C_PUNCT_IXOR    = 0x10A,  // ^=
-    C_PUNCT_INC     = 0x10B,  // ++
-    C_PUNCT_DEC     = 0x10C,  // --
-    C_PUNCT_RSHIFT  = 0x10D,  // >>
-    C_PUNCT_LSHIFT  = 0x10E,  // <<
-    C_PUNCT_LAND    = 0x11F,  // &&
-    C_PUNCT_LOR     = 0x110,  // ||
-    C_PUNCT_EQ      = 0x111,  // ==
-    C_PUNCT_NEQ     = 0x112,  // !=
-    C_PUNCT_LEQ     = 0x113,  // <=
-    C_PUNCT_GEQ     = 0x114,  // >=
-} c_punct_kind;
-
-typedef enum token_kind {
-    TOK_EOF   = 0x0,  // End of file
-    TOK_ID    = 0x1,  // Identfier
-    TOK_PUNCT = 0x2,  // Punctuator
-    TOK_STR   = 0x3,  // String or arbitrary type
-    TOK_NUM   = 0x4,  // Number (can be numeric literal or char literal)
-    TOK_KW    = 0x5,  // Keyword
-} token_kind;
-
-typedef struct token {
-    token_kind kind;
-    string str;
-    uint64_t uint_value;
-    long double float_value;
-    struct c_type *type;
-    c_punct_kind punct;
-    c_keyword_kind kw;
-} token;
-
 // #pragma once files
 // @NOTE: Number of files is generally small, so we are okay with having linked
 // list instead of hash map
@@ -171,8 +74,5 @@ typedef struct preprocessor {
 } preprocessor;
 
 void do_pp(preprocessor *pp);
-
-uint32_t fmt_token(token *tok, char *buf, uint32_t buf_size);
-uint32_t fmt_token_verbose(token *tok, char *buf, uint32_t buf_size);
 
 #endif
