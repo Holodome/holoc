1badd8c7480d9b3524b3a4daa3b47516d9c1e005
holodome <ilal.vinogradov@gmail.com>
2022-01-22 21:48:41 +0300

    rewrite execution of program modes that are chosen via command line in main.c

--- src/main.c
+++ src/main.c
@@ -14,28 +14,167 @@
 #include "preprocessor.h"
 #include "str.h"
 
-typedef struct {
+typedef enum {
     // print preprocessing tokens verbose, its on its own line
-    bool ptv;
-    // print preprocessing tokens verbose, as if they were on logical source
-    // lines
-    bool ptvf;
+    M_PTV,
     // print preprocessing tokens, as if they were on logical source lines
-    bool ptf;
+    M_PTF,
     // print preprocessing tokens, each on its own line
-    bool pt;
+    M_PT,
     // print c tokens, each on its own line
-    bool tp;
+    M_TP,
     // print c tokens verbose, each on its own line
-    bool tpv;
-    // dump ast tree in tree format
-    bool ast;
+    M_TPV,
+    // print c tokens, as if they were on logical source lines
+    M_TPF,
+    // print ast tree
+    M_AST,
+} mode;
+
+typedef struct {
+    string *filenames;  // da
+    mode mode;
 } program_settings;
 
 static program_settings settings;
 
 static void
-process_file(string filename) {
+parse_clargs(uint32_t argc, char **argv) {
+    allocator *a      = get_system_allocator();
+    string *filenames = 0;
+    for (uint32_t arg_idx = 1; arg_idx < argc; arg_idx++) {
+        if (strcmp(argv[arg_idx], "--ptv") == 0) {
+            settings.mode = M_PTV;
+        } else if (strcmp(argv[arg_idx], "--ptf") == 0) {
+            settings.mode = M_PTF;
+        } else if (strcmp(argv[arg_idx], "--pt") == 0) {
+            settings.mode = M_PT;
+        } else if (strcmp(argv[arg_idx], "--tp") == 0) {
+            settings.mode = M_TP;
+        } else if (strcmp(argv[arg_idx], "--tpv") == 0) {
+            settings.mode = M_TPV;
+        } else if (strcmp(argv[arg_idx], "--tpf") == 0) {
+            settings.mode = M_TPF;
+        } else if (strcmp(argv[arg_idx], "--ast") == 0) {
+            settings.mode = M_AST;
+        } else {
+            da_push(filenames, stringz(argv[arg_idx]), a);
+        }
+    }
+    settings.filenames = filenames;
+}
+
+static void
+mptv(string filename) {
+    allocator *a    = get_system_allocator();
+    file_storage fs = {0};
+    fs.a            = a;
+    add_default_include_paths(&fs);
+
+    file *f = get_file(&fs, filename, 0);
+
+    char token_buffer[4096];
+    pp_lexer *lex = aalloc(a, sizeof(*lex));
+    init_pp_lexer(lex, f->contents.data, STRING_END(f->contents), token_buffer,
+                  sizeof(token_buffer));
+    while (pp_lexer_parse(lex)) {
+        char buffer[4096];
+        fmt_pp_tok_verbose(buffer, sizeof(buffer), &lex->tok);
+        printf("%s\n", buffer);
+    }
+}
+
+static void
+mptf(string filename) {
+    allocator *a    = get_system_allocator();
+    file_storage fs = {0};
+    fs.a            = a;
+    add_default_include_paths(&fs);
+
+    file *f = get_file(&fs, filename, 0);
+
+    char token_buffer[4096];
+    pp_lexer *lex = aalloc(a, sizeof(*lex));
+    init_pp_lexer(lex, f->contents.data, STRING_END(f->contents), token_buffer,
+                  sizeof(token_buffer));
+    while (pp_lexer_parse(lex)) {
+        char buffer[4096];
+        fmt_pp_tok(buffer, sizeof(buffer), &lex->tok);
+        if (lex->tok.at_line_start) {
+            printf("\n");
+        } else if (lex->tok.has_whitespace) {
+            printf(" ");
+        }
+        printf("%s", buffer);
+    }
+}
+
+static void
+mpt(string filename) {
+    allocator *a    = get_system_allocator();
+    file_storage fs = {0};
+    fs.a            = a;
+    add_default_include_paths(&fs);
+
+    file *f = get_file(&fs, filename, 0);
+
+    char token_buffer[4096];
+    pp_lexer *lex = aalloc(a, sizeof(*lex));
+    init_pp_lexer(lex, f->contents.data, STRING_END(f->contents), token_buffer,
+                  sizeof(token_buffer));
+    while (pp_lexer_parse(lex)) {
+        char buffer[4096];
+        fmt_pp_tok(buffer, sizeof(buffer), &lex->tok);
+        printf("%s\n", buffer);
+    }
+}
+
+static void
+mtp(string filename) {
+    allocator *a = get_system_allocator();
+
+    file_storage fs = {0};
+    fs.a            = a;
+    add_default_include_paths(&fs);
+
+    bump_allocator ba = {0};
+    preprocessor *pp  = aalloc(a, sizeof(*pp));
+    pp->a             = &ba;
+    pp->ea            = a;
+    pp->fs            = &fs;
+
+    token *toks = do_pp(pp, filename);
+    for (token *tok = toks; tok; tok = tok->next) {
+        char buffer[4096];
+        fmt_token(tok, buffer, sizeof(buffer));
+        printf("%s\n", buffer);
+    }
+}
+
+static void
+mtpv(string filename) {
+    allocator *a = get_system_allocator();
+
+    file_storage fs = {0};
+    fs.a            = a;
+    add_default_include_paths(&fs);
+
+    bump_allocator ba = {0};
+    preprocessor *pp  = aalloc(a, sizeof(*pp));
+    pp->a             = &ba;
+    pp->ea            = a;
+    pp->fs            = &fs;
+
+    token *toks = do_pp(pp, filename);
+    for (token *tok = toks; tok; tok = tok->next) {
+        char buffer[4096];
+        fmt_token_verbose(tok, buffer, sizeof(buffer));
+        printf("%s\n", buffer);
+    }
+}
+
+static void
+mtpf(string filename) {
     allocator *a = get_system_allocator();
 
     file_storage fs = {0};
@@ -62,194 +201,57 @@ process_file(string filename) {
 }
 
 static void
-ptv(string filename) {
-    allocator *a    = get_system_allocator();
-    file_storage fs = {0};
-    fs.a            = a;
-    add_default_include_paths(&fs);
-
-    file *f = get_file(&fs, filename, 0);
-
-    char token_buffer[4096];
-    pp_lexer *lex = aalloc(a, sizeof(*lex));
-    init_pp_lexer(lex, f->contents.data, STRING_END(f->contents), token_buffer,
-                  sizeof(token_buffer));
-    while (pp_lexer_parse(lex)) {
-        char buffer[4096];
-        fmt_pp_tok_verbose(buffer, sizeof(buffer), &lex->tok);
-        printf("%s\n", buffer);
-    }
+mast(string filename) {
+    NOT_IMPL;
 }
-
-#if 0
 
 static void
 process_file(string filename) {
-    allocator *a     = get_system_allocator();
+    switch (settings.mode) {
+    default:
+        NOT_IMPL;
+        break;
+    case M_PTV:
+        mptv(filename);
+        break;
+    case M_PTF:
+        mptf(filename);
+        break;
+    case M_PT:
+        mpt(filename);
+        break;
 
-    if (settings.ptv) {
-        char lexer_buffer[4096];
-        pp_lexer lexer         = {0};
-        lexer.tok_buf          = lexer_buffer;
-        lexer.tok_buf_capacity = sizeof(lexer_buffer);
-        lexer.data             = file_contents;
-        l,exer.eof              = file_contents + strlen(file_contents);
-        lexer.cursor           = lexer.data;
-        while (pp_lexer_parse(&lexer)) {
-            char token_buf[4096];
-            fmt_pp_tok_verbose(&lexer.tok, token_buf, sizeof(token_buf));
-            printf("%s\n", token_buf);
-        }
-        return;
-    }
-
-    if (settings.ptvf) {
-        char lexer_buffer[4096];
-        pp_lexer lexer         = {0};
-        lexer.tok_buf          = lexer_buffer;
-        lexer.tok_buf_capacity = sizeof(lexer_buffer);
-        lexer.data             = file_contents;
-        lexer.eof              = file_contents + strlen(file_contents);
-        lexer.cursor           = lexer.data;
-        while (pp_lexer_parse(&lexer)) {
-            char token_buf[4096];
-            fmt_pp_tok_verbose(&lexer.tok, token_buf, sizeof(token_buf));
-            if (lexer.tok.at_line_start) {
-                printf("\n");
-            } else if (lexer.tok.has_whitespace) {
-                printf(" ");
-            }
-            printf("%s", token_buf);
-        }
-        return;
-    }
-
-    if (settings.ptf) {
-        char lexer_buffer[4096];
-        pp_lexer lexer         = {0};
-        lexer.tok_buf          = lexer_buffer;
-        lexer.tok_buf_capacity = sizeof(lexer_buffer);
-        lexer.data             = file_contents;
-        lexer.eof              = file_contents + strlen(file_contents);
-        lexer.cursor           = lexer.data;
-        while (pp_lexer_parse(&lexer)) {
-            char token_buf[4096];
-            fmt_pp_tok(&lexer.tok, token_buf, sizeof(token_buf));
-            if (lexer.tok.at_line_start) {
-                printf("\n");
-            } else if (lexer.tok.has_whitespace) {
-                printf(" ");
-            }
-            printf("%s", token_buf);
-        }
-        return;
-    }
-
-    if (settings.pt) {
-        char lexer_buffer[4096];
-        pp_lexer lexer         = {0};
-        lexer.tok_buf          = lexer_buffer;
-        lexer.tok_buf_capacity = sizeof(lexer_buffer);
-        lexer.data             = file_contents;
-        lexer.eof              = file_contents + strlen(file_contents);
-        lexer.cursor           = lexer.data;
-        while (pp_lexer_parse(&lexer)) {
-            char token_buf[4096];
-            fmt_pp_tok(&lexer.tok, token_buf, sizeof(token_buf));
-            printf("%s\n", token_buf);
-        }
-        return;
-    }
-
-    bump_allocator pp_bump = {0};
-    preprocessor *pp       = bump_alloc(&pp_bump, sizeof(preprocessor));
-    pp->lex                = &lexer;
-    pp->a                  = &pp_bump;
-    pp->ea                 = a;
-    token *toks            = do_pp(pp);
-
-    if (settings.tp) {
-        for (token *tok = toks; tok; tok = tok->next) {
-            char buffer[4096];
-            fmt_token(tok, buffer, sizeof(buffer));
-            printf("%s\n", buffer);
-        }
-        return;
-    }
-
-    if (settings.tpv) {
-        for (token *tok = toks; tok; tok = tok->next) {
-            char buffer[4096];
-            fmt_token_verbose(tok, buffer, sizeof(buffer));
-            printf("%s\n", buffer);
-        }
-        return;
-    }
-
-    bump_allocator ast_b_bump = {0};
-    ast_builder *ast_b        = bump_alloc(&ast_b_bump, sizeof(ast_builder));
-    ast_b->a                  = &ast_b_bump;
-    ast_b->ea                 = a;
-    ast_b->toks               = toks;
-    ast_b->tok                = toks;
-
-    if (settings.ast) {
-        for (;;) {
-            ast *toplevel = build_toplevel_ast(ast_b);
-            if (!toplevel) {
-                break;
-            }
-            char buffer[16384];
-            fmt_ast_verbose(toplevel, buffer, sizeof(buffer));
-            printf("%s", buffer);
-        }
-        return;
+    case M_TPV:
+        mtpv(filename);
+        break;
+    case M_TPF:
+        mtpf(filename);
+        break;
+    case M_TP:
+        mtp(filename);
+        break;
+    case M_AST:
+        mast(filename);
+        break;
     }
 }
 
-#endif
-
 int
 main(int argc, char **argv) {
-    allocator *a = get_system_allocator();
+    parse_clargs(argc, argv);
 
-    string *filenames = 0;
-    for (int arg_idx = 1; arg_idx < argc; arg_idx++) {
-        if (strcmp(argv[arg_idx], "--ptv") == 0) {
-            settings.ptv = true;
-        } else if (strcmp(argv[arg_idx], "--ptvf") == 0) {
-            settings.ptvf = true;
-        } else if (strcmp(argv[arg_idx], "--ptf") == 0) {
-            settings.ptf = true;
-        } else if (strcmp(argv[arg_idx], "--pt") == 0) {
-            settings.pt = true;
-        } else if (strcmp(argv[arg_idx], "--tp") == 0) {
-            settings.tp = true;
-        } else if (strcmp(argv[arg_idx], "--tpv") == 0) {
-            settings.tpv = true;
-        } else if (strcmp(argv[arg_idx], "--ast") == 0) {
-            settings.ast = true;
-        } else {
-            da_push(filenames, stringz(argv[arg_idx]), a);
-        }
-    }
-
-    /* da_push(filenames, WRAP_Z("examples/example.c"), a); */
-    /* da_push(filenames, WRAP_Z("examples/pp/a.h"), a); */
-
-    if (!da_size(filenames)) {
+    if (!da_size(settings.filenames)) {
         fprintf(stderr, "error: no input files");
         return 1;
     }
 
-    for (uint32_t filename_idx = 0; filename_idx < da_size(filenames);
+    for (uint32_t filename_idx = 0; filename_idx < da_size(settings.filenames);
          filename_idx++) {
-        string filename = filenames[filename_idx];
+        string filename = settings.filenames[filename_idx];
         printf("Filename %u: %.*s\n", filename_idx, filename.len,
                filename.data);
 
-        /* process_file(filename); */
-        ptv(filename);
+        process_file(filename);
     }
 
     return 0;
